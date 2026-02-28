64528268e3580fe68587e588be9f076d2ac8949c
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-02-27 22:54:33 -0600

    feat: add RefreshToken model

---
+++ server/FacilityOS.API/FacilityOS.API/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacilityOS.API.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRevoked { get; set; } = false;
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+
+}
