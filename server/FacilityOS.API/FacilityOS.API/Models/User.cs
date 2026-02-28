03a68e6af3e06c0780ddb181c54bccdfeaf9bfd3
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-02-27 22:52:28 -0600

    feat: add User model

---
+++ server/FacilityOS.API/FacilityOS.API/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacilityOS.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string Role { get; set; } = "User"; 
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
