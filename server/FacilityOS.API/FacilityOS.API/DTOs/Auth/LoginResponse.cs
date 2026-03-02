c7b5ce3b974dc14f6bf156c4b7c8ac6112754487
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-01 22:58:08 -0600

    feat: add LoginResponse

---
+++ server/FacilityOS.API/FacilityOS.API/DTOs/Auth/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FacilityOS.API.DTOs.Auth
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public UserDto User { get; set; } = null!;
+    }
+}
