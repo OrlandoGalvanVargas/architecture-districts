678f11d1d7d3f886954d7becebb9e14384dbf07a
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-02 22:57:31 -0600

    feat: update UserDto

--- server/FacilityOS.API/FacilityOS.API/DTOs/Auth/UserDto.cs
+++ server/FacilityOS.API/FacilityOS.API/DTOs/Auth/UserDto.cs
@@ -1,9 +1,13 @@
-﻿namespace FacilityOS.API.DTOs.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacilityOS.API.DTOs.Auth
 {
     public class UserDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
     }
