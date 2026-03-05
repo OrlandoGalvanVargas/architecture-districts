6b2f669cf52702971c144f7a64a5954cff78823d
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-04 22:45:22 -0600

    feat: add RefreshTokenRequest in DTOs

---
+++ server/FacilityOS.API/FacilityOS.API/DTOs/Auth/RefreshTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacilityOS.API.DTOs.Auth
+{
+    public class RefreshTokenRequest
+    {
+        [Required]
+        public string Token { get; set; } = string.Empty;
+    }
+}
