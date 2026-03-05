db5c398f71eda8c3545289df7f32c5bd450b0050
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-04 22:35:37 -0600

    feat: add UserDto into LoginReponse to DTOs

--- server/FacilityOS.API/FacilityOS.API/DTOs/Auth/LoginResponse.cs
+++ server/FacilityOS.API/FacilityOS.API/DTOs/Auth/LoginResponse.cs
@@ -6,4 +6,13 @@
         public string RefreshToken { get; set; } = string.Empty;
         public UserDto User { get; set; } = null!;
     }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+
 }
