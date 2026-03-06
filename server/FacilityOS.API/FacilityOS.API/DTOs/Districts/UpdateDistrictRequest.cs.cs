2045ea5fdeacbf3f0ea0b617186fb3bff5fff82b
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-05 22:58:01 -0600

    feat: add UpdateDistrictRequest base

---
+++ server/FacilityOS.API/FacilityOS.API/DTOs/Districts/UpdateDistrictRequest.cs.cs
@@ -0,0 +1,7 @@
+﻿namespace FacilityOS.API.DTOs.Districts
+{
+    public class UpdateDistrictRequest
+    {
+        // Client UpdateDistrictRequest should not include Id, as it is passed in the URL
+    }
+}
