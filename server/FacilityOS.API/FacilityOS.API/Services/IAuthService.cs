9451bc16fbbc3401d329b86728b437ae323d5019
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-03-07 22:54:05 -0600

    feat: add IAuthService base v2

--- server/FacilityOS.API/FacilityOS.API/Services/IAuthService.cs
+++ server/FacilityOS.API/FacilityOS.API/Services/IAuthService.cs
@@ -2,5 +2,23 @@
 {
     public interface IAuthService
     {
+        /// <summary>
+        /// Generates a JWT token for the given user.
+        /// </summary>
+        /// <param name="userId">The ID of the user.</param>
+        /// <returns>A JWT token as a string.</returns>
+        string GenerateToken(string userId);
+        /// <summary>
+        /// Validates the provided JWT token.
+        /// </summary>
+        /// <param name="token">The JWT token to validate.</param>
+        /// <returns>True if the token is valid; otherwise, false.</returns>
+        bool ValidateToken(string token);
+        /// <summary>
+        /// Gets the user ID from the provided JWT token.
+        /// </summary>
+        /// <param name="token">The JWT token.</param>
+        /// <returns>The user ID if the token is valid; otherwise, null.</returns>
+        string? GetUserIdFromToken(string token);
     }
 }
