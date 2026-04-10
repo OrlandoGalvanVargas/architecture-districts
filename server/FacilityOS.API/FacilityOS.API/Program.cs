636c34514a75eef25d9dffb1b665320605db4c87
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-04-09 22:58:53 -0600

    feat: add new MapPut to WeatherForecast

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -46,8 +46,7 @@ app.MapGet("/items", () => Results.Ok("List of items"));
 app.MapPost("/create", (string value) => Results.Ok($"Created item with value: {value}"));
 app.MapDelete("/items/{id}", (int id) => Results.Ok($"Deleted item with ID: {id}"));
 app.MapGet("/items/{id}", (int id) => Results.Ok($"Item with ID: {id}"));
-app.MapPut("/items/{id}", (int id, string value) => Results.Ok($"Updated item with ID: {id} to value: {value}"));
-
+app.MapPut("", (int id, string value) => Results.Ok($"Updated item with ID: {id} to value: {value}"));
 
 app.Run();
 
