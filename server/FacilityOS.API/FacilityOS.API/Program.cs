e794b79392f52378c8d9383b2d3f9628ad2c4bf9
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-04-11 22:54:21 -0600

    feat: add new MapGet users

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -26,7 +26,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering",
 };
 
-app.MapGet("/", () => "Hello World!").WithName("HelloWorld").WithOpenApi(); 
+app.MapGet("/users", () => Results.Ok("List of users"));       
+app.MapGet("/users/{id}", (int id) => Results.Ok($"User with ID: {id}"));   
 
 app.MapGet("/weatherforecast", () =>
 {
