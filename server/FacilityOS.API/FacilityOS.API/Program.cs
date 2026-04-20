774dcf28de226f78e10dc01232be2637bee9f780
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-04-19 22:59:57 -0600

    feat: add MapPatch users

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -56,7 +56,7 @@ app.MapGet("/users/{userId}/orders/{orderId}"   , (int userId, int orderId) => R
 app.MapPost("/users/{userId}/orders", (int userId, string orderDetails) => Results.Ok($"Created order for User ID: {userId} with details: {orderDetails}"));    
 app.MapPut("/users/{userId}/orders/{orderId}", (int userId, int orderId, string orderDetails) => Results.Ok($"Updated order with ID: {orderId} for User ID: {userId} with details: {orderDetails}"));
 app.MapDelete("/users/{userId}/orders/{orderId}", (int userId, int orderId) => Results.Ok($"Deleted order with ID: {orderId} for User ID: {userId}"));  
-
+app.MapPatch("/users/{userId}/orders/{orderId}", (int userId, int orderId, string orderDetails) => Results.Ok($"Partially updated order with ID: {orderId} for User ID: {userId} with details: {orderDetails}"));   
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
