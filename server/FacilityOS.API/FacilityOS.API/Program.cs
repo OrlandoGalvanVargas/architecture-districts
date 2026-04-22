f02cde93a0e5b18d08a13408ba88ca111cb19cb7
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-04-21 22:47:21 -0600

    feat: add MapPost to products

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -58,6 +58,7 @@ app.MapPut("/users/{userId}/orders/{orderId}", (int userId, int orderId, string 
 app.MapDelete("/users/{userId}/orders/{orderId}", (int userId, int orderId) => Results.Ok($"Deleted order with ID: {orderId} for User ID: {userId}"));  
 app.MapPatch("/users/{userId}/orders/{orderId}", (int userId, int orderId, string orderDetails) => Results.Ok($"Partially updated order with ID: {orderId} for User ID: {userId} with details: {orderDetails}"));   
 app.MapGet("/products/{productId}/reviews", (int productId) => Results.Ok($"Reviews for Product ID: {productId}"));     
+app.MapPost("/products/{productId}/reviews", (int productId, string reviewDetails) => Results.Ok($"Created review for Product ID: {productId} with details: {reviewDetails}"));     
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
