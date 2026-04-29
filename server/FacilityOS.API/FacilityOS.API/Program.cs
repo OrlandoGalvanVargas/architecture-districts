ea0b1377a39438e13fe0b17415dee9db2acc820f
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-04-28 22:59:50 -0600

    feat: add MapGetAll to users

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -49,7 +49,7 @@ app.MapPost("/users/{userId}/orders", (int userId, string orderDetails) => Resul
 app.MapPut("/users/{userId}/orders/{orderId}", (int userId, int orderId, string orderDetails) => Results.Ok($"Updated order with ID: {orderId} for User ID: {userId} with details: {orderDetails}"));
 app.MapDelete("/users/{userId}/orders/{orderId}", (int userId, int orderId) => Results.Ok($"Deleted order with ID: {orderId} for User ID: {userId}"));  
 app.MapPatch("/users/{userId}/orders/{orderId}", (int userId, int orderId, string orderDetails) => Results.Ok($"Partially updated order with ID: {orderId} for User ID: {userId} with details: {orderDetails}"));   
-
+app.MapGet("/users", () => Results.Ok("List of users")).WithName("GetUsers").WithOpenApi();
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
