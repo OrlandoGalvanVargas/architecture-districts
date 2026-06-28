c3ed31655cffa2abbb18ad8704cd9f8adbe2da63
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-06-28 01:44:46 -0600

    feat: add AuthController file to Controllers

---
+++ server/FacilityOS.API/FacilityOS.API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using FacilityOS.API.DTOs.Auth;
+using FacilityOS.API.Features.Auth.Login;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacilityOS.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest Request)
+        {
+            var result = await _mediator.Send(new LoginCommand(Request));
+            return Ok(result);
+        }
+
+    }
+}
