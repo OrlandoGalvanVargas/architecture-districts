7ce4e4827bb4369c1624199c0cad699038d5c8be
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-06-28 00:53:27 -0600

    feat: add LoginCommand to /Features/Auth/Login

---
+++ server/FacilityOS.API/FacilityOS.API/Features/Auth/Login/LoginCommand.cs
@@ -0,0 +1,7 @@
+﻿using FacilityOS.API.DTOs.Auth;
+using MediatR;
+
+namespace FacilityOS.API.Features.Auth.Login
+{
+    public record LoginCommand(LoginRequest Request) : IRequest<LoginResponse>;
+}
