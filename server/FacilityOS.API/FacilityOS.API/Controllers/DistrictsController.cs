a7819f4d59bcdd422e9bea05bb778ebe9f6306da
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-06-27 23:36:32 -0600

    feat: add GetById, Put and Delete endpoints to the DistrictsController

--- server/FacilityOS.API/FacilityOS.API/Controllers/DistrictsController.cs
+++ server/FacilityOS.API/FacilityOS.API/Controllers/DistrictsController.cs
@@ -1,6 +1,9 @@
 ﻿using FacilityOS.API.DTOs.Districts;
 using FacilityOS.API.Features.Districts.CreateDistrict;
+using FacilityOS.API.Features.Districts.DeleteDistrict;
+using FacilityOS.API.Features.Districts.GetDistrictById;
 using FacilityOS.API.Features.Districts.GetDistricts;
+using FacilityOS.API.Features.Districts.UpdateDistrict;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,11 +28,46 @@ namespace FacilityOS.API.Controllers
             return Ok(result);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DistrictResponse>> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetDistrictByIdQuery(id));
+            
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
         [HttpPost]
         public async Task<ActionResult<DistrictResponse>> Create([FromBody] CreateDistrictRequest request)
         {
             var result = await _mediator.Send(new CreateDistrictCommand(request));
-            return CreatedAtAction(nameof(GetAll), result);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<DistrictResponse>> Update(int id, [FromBody] UpdateDistrictRequest request)
+        {
+            var result = await _mediator.Send(new UpdateDistrictCommand(id, request));
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var deleted = await _mediator.Send(new DeleteDistrictCommand(id));
+
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+
         }
     }
 }
