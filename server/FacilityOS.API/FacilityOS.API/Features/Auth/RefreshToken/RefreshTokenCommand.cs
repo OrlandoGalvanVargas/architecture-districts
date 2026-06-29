a0933da129abfe81f370d70f020fa22211301712
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-06-29 00:48:15 -0600

    feat: add RefreshTokenCommand to /Features/Auth/RefreshToken

---
+++ server/FacilityOS.API/FacilityOS.API/Features/Auth/RefreshToken/RefreshTokenCommand.cs
@@ -0,0 +1,7 @@
+﻿using FacilityOS.API.DTOs.Auth;
+using MediatR;
+
+namespace FacilityOS.API.Features.Auth.RefreshToken
+{
+    public record RefreshTokenCommand(string RefreshToken) : IRequest<LoginResponse>;
+}
