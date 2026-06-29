36d1cf71646d5c135c21837a97cc7a6bda4f14a6
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-06-28 23:36:39 -0600

    feat: add LogoutHandler to Features/Auth/Logout

---
+++ server/FacilityOS.API/FacilityOS.API/Features/Auth/Logout/LogoutHandler.cs
@@ -0,0 +1,29 @@
+﻿using FacilityOS.API.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FacilityOS.API.Features.Auth.Logout
+{
+    public class LogoutHandler : IRequestHandler<LogoutCommand, bool>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogoutHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(LogoutCommand command, CancellationToken cancellationToken)
+        {
+            var token = await _context.RefreshTokens.FirstOrDefaultAsync(t => t.Token == command.RefreshToken, cancellationToken);
+
+            if (token is null)
+                return false;
+
+            token.IsRevoked = true;
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
