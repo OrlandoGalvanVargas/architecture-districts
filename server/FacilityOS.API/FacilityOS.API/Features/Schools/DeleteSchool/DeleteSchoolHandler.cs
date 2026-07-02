e3c2931526c34312dfdcc1122c5f13601bd5113b
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-02 15:56:32 -0600

    feat: add DeleteSchoolHandler to /Features/Schools/DeleteSchool

---
+++ server/FacilityOS.API/FacilityOS.API/Features/Schools/DeleteSchool/DeleteSchoolHandler.cs
@@ -0,0 +1,29 @@
+﻿using FacilityOS.API.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Net.WebSockets;
+
+namespace FacilityOS.API.Features.Schools.DeleteSchool
+{
+    public class DeleteSchoolHandler : IRequestHandler<DeleteSchoolCommand, bool>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteSchoolHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteSchoolCommand command, CancellationToken cancellationToken)
+        {
+            var school = await _context.Schools.FirstOrDefaultAsync(s => s.Id == command.Id, cancellationToken);
+
+            if (school is null)
+                return false;
+
+            _context.Remove(school);
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
