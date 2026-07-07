853fa54ae7d00b4451115a7c0496a3e1b0ea75ce
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-06 23:27:23 -0600

    feat: implement SchoolView component with routing for school pages

--- client/src/features/schools/SchoolView.jsx
+++ client/src/features/schools/SchoolView.jsx
@@ -1,1 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import { SchoolsPage } from "./pages/SchoolsPage";
+import { SchoolCreatePage } from "./pages/SchoolCreatePage";
+import { SchoolDetailPage } from "./pages/SchoolDetailPag";
+import { SchoolEditPage } from "./pages/SchoolEditPage";
+
+export const SchoolView = () => {
+  return (
+    <Routes>
+      <Route index element={<SchoolsPage />} />
+      <Route path="" element={<SchoolCreatePage />} />
+      <Route path=":id" element={<SchoolDetailPage />} />
+      <Route path=":id/edit" element={<SchoolEditPage />} />
+    </Routes>
+  );
+};
