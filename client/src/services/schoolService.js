799a732b37f2ea214857949967ef37700e5df0d1
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-06 22:59:14 -0600

    feat: implement school service with success and error handling

--- client/src/services/schoolService.js
+++ client/src/services/schoolService.js
@@ -1,1 +1,23 @@
+import { schoolsApi } from "../api/school/school.api";
 import { createService } from "../reactive/createService";
+
+const schoolReactor = {
+  onSuccess: ({ action, payload, db }) => {
+    switch (action) {
+      case "getAll":
+        db.collection("schools").bulkWrite(payload);
+        break;
+    }
+  },
+  onError: ({ action, error }) => {
+    switch (action) {
+      case "getAll":
+        console.log("Error fetching schools: ", error);
+        break;
+      default:
+        console.log(`Error in ${action}: `, error);
+    }
+  },
+};
+
+export const schoolService = createService(schoolsApi, schoolService);
