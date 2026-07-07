63b3a0d78fbdd2cd4a26ac4e68947248a086c510
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-06 23:00:28 -0600

    feat: add schoolService import to services index

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,7 @@
 import { districtService } from "./districtService";
+import { schoolService } from "./schoolService";
 
 export default {
   districts: districtService,
+  schools: schoolService,
 };
