52efbd2808a7e3c442dd6130a3f630cdfd22e00f
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-06 23:13:22 -0600

    feat: add schools route paths to RoutePaths configuration

--- client/src/router/RoutePaths.js
+++ client/src/router/RoutePaths.js
@@ -15,4 +15,17 @@ export const RoutePaths = {
     edit: (id) =>
       `${ROUTES_CONFIG.districts.path}/${ROUTES_CONFIG.districts.children.edit.buildPath(id)}`,
   },
+
+  schools: {
+    list: () => ROUTES_CONFIG.schools.path,
+
+    create: () =>
+      `${ROUTES_CONFIG.schools.path}/${ROUTES_CONFIG.schools.children.create.buildPath()}`,
+
+    detail: (id) =>
+      `${ROUTES_CONFIG.schools.path}/${ROUTES_CONFIG.schools.children.detail.buildPath(id)}`,
+
+    edit: (id) =>
+      `${ROUTES_CONFIG.schools.path}/${ROUTES_CONFIG.schools.children.edit.buildPath(id)}`,
+  },
 };
