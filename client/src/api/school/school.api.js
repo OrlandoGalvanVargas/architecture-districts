f117948ac37647b461e227a749e0aa40ef7a94dc
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-06 20:52:56 -0600

    feat: add new school.api.js complete

--- client/src/api/school/school.api.js
+++ client/src/api/school/school.api.js
@@ -1,1 +1,25 @@
 import { apiClient } from "../client";
+
+const ENDPOINT = "/schools";
+
+export const schoolsApi = {
+  getAll: async (params = {}) => {
+    const response = await apiClient.get(ENDPOINT, { params });
+    return response.data;
+  },
+  getById: async (id) => {
+    const response = await apiClient.get(`${ENDPOINT}/${id}`);
+    return response.data;
+  },
+  create: async (data) => {
+    const response = await apiClient.post(ENDPOINT, data);
+    return response.data;
+  },
+  update: async (id, data) => {
+    const response = await apiClient.put(`${ENDPOINT}/${id}`, data);
+    return response.data;
+  },
+  delete: async (id) => {
+    await apiClient.delete(`${ENDPOINT}/${id}`);
+  },
+};
