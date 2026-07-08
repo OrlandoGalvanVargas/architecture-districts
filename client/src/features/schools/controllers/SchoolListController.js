2a1e21805b0d93759c5cbb5e84c0e0931419c7a7
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-07 23:45:51 -0600

    feat: add SchoolListController with initial imports and setup

---
+++ client/src/features/schools/controllers/SchoolListController.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from "react";
+import { withController } from "@/reactive/withController";
+import { SchoolTable } from "../components/SchoolTable";
+import services from "@/services";
