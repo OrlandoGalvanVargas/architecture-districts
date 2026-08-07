4bfd9ca81b6fa8859085de2cb499544ebc4b830e
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-06 22:26:09 -0600

    feat: update routing in SchoolView component to use ROUTES_CONFIG

--- client/src/features/schools/SchoolView.jsx
+++ client/src/features/schools/SchoolView.jsx
@@ -1,16 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { SchoolsPage } from "./pages/SchoolsPage";
 import { SchoolCreatePage } from "./pages/SchoolCreatePage";
 import { SchoolDetailPage } from "./pages/SchoolDetailPage";
 import { SchoolEditPage } from "./pages/SchoolEditPage";
+import { ROUTES_CONFIG } from "@/router/routes";
 
 export const SchoolView = () => {
+  const routes = ROUTES_CONFIG.schools.children;
+
   return (
     <Routes>
       <Route index element={<SchoolsPage />} />
-      <Route path="" element={<SchoolCreatePage />} />
-      <Route path=":id" element={<SchoolDetailPage />} />
-      <Route path=":id/edit" element={<SchoolEditPage />} />
+      <Route path={routes.create.pattern} element={<SchoolCreatePage />} />
+      <Route path={routes.detail.pattern} element={<SchoolDetailPage />} />
+      <Route path={routes.edit.pattern} element={<SchoolEditPage />} />
+
+      <Route
+        path="*"
+        element={<Navigate to={ROUTES_CONFIG.schools.path} replace />}
+      />
     </Routes>
   );
 };
