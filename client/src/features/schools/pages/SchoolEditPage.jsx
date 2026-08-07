5b4a1182dfef04696ca4671180579ec565348c82
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-07 12:53:59 -0600

    feat: enhance SchoolEditPage with breadcrumbs and PageHeader

--- client/src/features/schools/pages/SchoolEditPage.jsx
+++ client/src/features/schools/pages/SchoolEditPage.jsx
@@ -1,3 +1,16 @@
-import SchoolEditController from "../controllers/SchoolEditController";
+import { SchoolEditController } from "../controllers/SchoolEditController";
+import { PageHeader } from "@/components/Layout/PageHeader/PageHeader";
+import { generateBreadcrumbs } from "@/router/breadcrumbs";
+import { useParams } from "react-router-dom";
 
-export const SchoolEditPage = () => <SchoolEditController />;
+export const SchoolEditPage = () => {
+  const { id } = useParams();
+  const breadcrumbs = generateBreadcrumbs("schools", "edit", { id });
+
+  return (
+    <div>
+      <PageHeader title="Edit School" breadcrumbs={breadcrumbs} />
+      <SchoolEditController schoolId={id} />
+    </div>
+  );
+};
