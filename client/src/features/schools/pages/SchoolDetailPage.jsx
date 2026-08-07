4ced1987e30e5956032348f92ee255e509df238a
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-07 12:50:51 -0600

    feat: enhance SchoolDetailPage with breadcrumbs and PageHeader

--- client/src/features/schools/pages/SchoolDetailPage.jsx
+++ client/src/features/schools/pages/SchoolDetailPage.jsx
@@ -1,3 +1,16 @@
-import SchoolDetailController from "../controllers/SchoolDetailController";
+import { SchoolDetailController } from "../controllers/SchoolDetailController";
+import { PageHeader } from "@/components/Layout/PageHeader/PageHeader";
+import { generateBreadcrumbs } from "@/router/breadcrumbs";
+import { useParams } from "react-router-dom";
 
-export const SchoolDetailPage = () => <SchoolDetailController />;
+export const SchoolDetailPage = () => {
+  const { id } = useParams();
+  const breadcrumbs = generateBreadcrumbs("schools", "detail", { id });
+
+  return (
+    <div>
+      <PageHeader title="School Details" breadcrumbs={breadcrumbs} />
+      <SchoolDetailController schoolId={id} />
+    </div>
+  );
+};
