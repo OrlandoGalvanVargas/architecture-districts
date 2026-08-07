560a7a2e8a2cdf84702813933ff4250c507279d5
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-07 12:37:25 -0600

    feat: refactor SchoolCreatePage to include breadcrumbs and PageHeader

--- client/src/features/schools/pages/SchoolCreatePage.jsx
+++ client/src/features/schools/pages/SchoolCreatePage.jsx
@@ -1,3 +1,14 @@
-import SchoolCreateController from "../controllers/SchoolCreateController";
+import { SchoolCreateController } from "../controllers/SchoolCreateController";
+import { PageHeader } from "@/components/Layout/PageHeader/PageHeader";
+import { generateBreadcrumbs } from "@/router/breadcrumbs";
 
-export const SchoolCreatePage = () => <SchoolCreateController />;
+export const SchoolCreatePage = () => {
+  const breadcrumbs = generateBreadcrumbs("schools", "create");
+
+  return (
+    <div>
+      <PageHeader title="Create School" breadcrumbs={breadcrumbs} />
+      <SchoolCreateController />
+    </div>
+  );
+};
