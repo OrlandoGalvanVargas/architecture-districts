20f2eaca16d0247b42df3c733b1e561ef0aab8fd
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-06 22:34:40 -0600

    feat: enhance SchoolsPage component with breadcrumbs and PageHeader

--- client/src/features/schools/pages/SchoolsPage.jsx
+++ client/src/features/schools/pages/SchoolsPage.jsx
@@ -1,3 +1,14 @@
-import SchoolListController from "../controllers/SchoolListController";
+import { SchoolListController } from "../controllers/SchoolListController";
+import { PageHeader } from "@/components/Layout/PageHeader/PageHeader";
+import { generateBreadcrumbs } from "@/router/breadcrumbs";
 
-export const SchoolsPage = () => <SchoolListController />;
+export const SchoolsPage = () => {
+  const breadcrumbs = generateBreadcrumbs("schools", "list");
+
+  return (
+    <div>
+      <PageHeader title="Schools" breadcrumbs={breadcrumbs} />
+      <SchoolListController />
+    </div>
+  );
+};
