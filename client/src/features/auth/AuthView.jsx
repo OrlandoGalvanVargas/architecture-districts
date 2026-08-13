3c52e7258e0cc8775b25ec6778818e8bfa800950
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-12 18:42:45 -0600

    feat: update AuthView to use ROUTES_CONFIG for login navigation

--- client/src/features/auth/AuthView.jsx
+++ client/src/features/auth/AuthView.jsx
@@ -1,11 +1,24 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
+import { ROUTES_CONFIG } from "@/router/routes";
 
 export const AuthView = () => {
   return (
     <Routes>
-      <Route index path="login" element={<LoginPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route
+        index
+        path={ROUTES_CONFIG.auth.children.login.pattern}
+        element={<LoginPage />}
+      />
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to={`/${ROUTES_CONFIG.auth.children.login.pattern}`}
+            replace
+          />
+        }
+      />
     </Routes>
   );
 };
