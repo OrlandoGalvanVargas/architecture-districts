28aed3c55392094b83eaa8911df9b561f73bdac2
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-17 23:57:28 -0600

    feat(validators): add CreateDistrictRequestValidator, UpdateDistrictRequestValidator, RefreshTokenRequestValidator, and UpdateSchoolRequestValidator for request validation

---
+++ server/FacilityOS.API/FacilityOS.API/Validators/RefreshTokenRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FacilityOS.API.DTOs.Auth;
+using FluentValidation;
+
+namespace FacilityOS.API.Validators;
+
+public class RefreshTokenRequestValidator : AbstractValidator<RefreshTokenRequest>
+{
+    public RefreshTokenRequestValidator()
+    {
+        RuleFor(x => x.Token)
+            .NotEmpty().WithMessage("Token is required");
+
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty().WithMessage("RefreshToken is required");
+    }
+}
