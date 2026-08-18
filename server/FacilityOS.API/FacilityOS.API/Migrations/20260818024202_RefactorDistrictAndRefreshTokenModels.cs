f1cf8b50de771f35f495a823be23f236dd5326ac
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-17 23:59:07 -0600

    refactor(migrations): remove unused columns and update entity type in Users table

---
+++ server/FacilityOS.API/FacilityOS.API/Migrations/20260818024202_RefactorDistrictAndRefreshTokenModels.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityOS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorDistrictAndRefreshTokenModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsExpired",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolCount",
+                table: "Districts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityType",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EntityType",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExpired",
+                table: "RefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SchoolCount",
+                table: "Districts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Districts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SchoolCount",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Districts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SchoolCount",
+                value: 0);
+        }
+    }
+}
