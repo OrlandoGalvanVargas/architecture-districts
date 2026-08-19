0236528589dc67ff4a8d8dad8b6e43b48f41e804
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-19 01:30:26 -0600

    feat(users): add IsActive and IsDeleted columns to Users table for enhanced user state management

---
+++ server/FacilityOS.API/FacilityOS.API/Migrations/20260819055822_AddUserStatusAndSoftDelete.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityOS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStatusAndSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+        }
+    }
+}
