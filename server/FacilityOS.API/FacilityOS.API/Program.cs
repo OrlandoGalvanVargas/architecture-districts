c3b74133769afeef4fb4cfb7dede7a932a133dbb
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-20 23:26:35 -0600

    feat: update Program.cs

--- server/FacilityOS.API/FacilityOS.API/Program.cs
+++ server/FacilityOS.API/FacilityOS.API/Program.cs
@@ -1,17 +1,18 @@
-using FacilityOS.API.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using FacilityOS.API.Services;
+using FacilityOS.API.Common;
 using FacilityOS.API.Common.Exceptions;
-using Microsoft.AspNetCore.RateLimiting;
-using System.Threading.RateLimiting;
+using FacilityOS.API.Data;
+using FacilityOS.API.Services;
+using FacilityOS.API.Settings;
+using FacilityOS.API.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using FacilityOS.API.Validators;
-using FacilityOS.API.Settings;
-using FacilityOS.API.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +24,20 @@ builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateSchoolRequestValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateSchoolRequestValidator>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", document)] = []
+    });
+});
 
 // 2. Base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -82,13 +96,15 @@ else if (builder.Environment.IsDevelopment())
 // 7. Rate Limiting
 builder.Services.AddRateLimiter(options =>
 {
-    options.AddFixedWindowLimiter("global", limiterOptions =>
-    {
-        limiterOptions.PermitLimit = permitLimit;
-        limiterOptions.Window = TimeSpan.FromMinutes(windowMinutes);
-        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-        limiterOptions.QueueLimit = 0;
-    });
+    options.AddPolicy("global", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "anonymous",
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = permitLimit,
+                Window = TimeSpan.FromMinutes(windowMinutes),
+                QueueLimit = 0
+            }));
 });
 
 // 8. Autenticación JWT
@@ -104,7 +120,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidIssuer = jwtSettings.Issuer,
             ValidAudience = jwtSettings.Audience,
             IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(jwtSettings.Key))
+                Encoding.UTF8.GetBytes(jwtSettings.Key)),
         };
     });
 
@@ -142,7 +158,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "FacilityOS API v1");
+        options.EnablePersistAuthorization();
+    });
 }
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
