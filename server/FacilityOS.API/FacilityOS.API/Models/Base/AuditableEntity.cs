75dc694a0299f1d706bdd7f874c285ca54645259
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-22 23:02:23 -0600

    feat: add base files to models

---
+++ server/FacilityOS.API/FacilityOS.API/Models/Base/AuditableEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace FacilityOS.API.Models.Base;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public bool IsActive { get; protected set; } = true;
+    public bool IsDeleted { get; protected set; } = false;
+
+    public void SoftDelete()
+    {
+        IsDeleted = true;
+        IsActive = false;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+        IsActive = true;
+    }
+
+    public void Activate() => IsActive = true;
+    public void Deactivate() => IsActive = false;
+}
