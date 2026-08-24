60e8749ed6145ba6b14a1f20aa5c55964765d006
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-23 22:43:29 -0600

    feat: add Enums files to Models

---
+++ server/FacilityOS.API/FacilityOS.API/Models/Enums/SchoolType.cs
@@ -0,0 +1,10 @@
+﻿namespace FacilityOS.API.Models.Enums
+{
+    public enum SchoolType
+    {
+        Public,
+        Charter,
+        Magnet,
+        Alternative
+    }
+}
---
+++ server/FacilityOS.API/FacilityOS.API/Models/Enums/UserEntityType.cs
@@ -0,0 +1,9 @@
+﻿namespace FacilityOS.API.Models.Enums
+{
+    public enum UserEntityType
+    {
+        Global = 0,   
+        District = 1,
+        School = 2 
+    }
+}
