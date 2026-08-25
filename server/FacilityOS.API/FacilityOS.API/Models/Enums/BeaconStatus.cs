bebde41896641a9a7f9d1c40898750bf22afc0cd
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-24 23:58:07 -0600

    feat: add new Enum files

---
+++ server/FacilityOS.API/FacilityOS.API/Models/Enums/BeaconStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FacilityOS.API.Models.Enums;
+
+public enum BeaconStatus
+{
+    Available = 1,     // Disponible para asignar
+    Assigned = 2,      // Asignado a entidad/persona
+    Maintenance = 3,   // En mantenimiento
+    Inactive = 4       // Inactivo/Descontinuado
+}
---
+++ server/FacilityOS.API/FacilityOS.API/Models/Enums/BeaconType.cs
@@ -0,0 +1,9 @@
+﻿namespace FacilityOS.API.Models.Enums;
+
+public enum BeaconType
+{
+    Pendant = 1,      // Colgante
+    Wristband = 2,    // Pulsera
+    Fixed = 3,        // Fijo en pared
+    Mobile = 4        // Móvil
+}
