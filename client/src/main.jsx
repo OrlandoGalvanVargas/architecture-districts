6023dcc1113797738a33cf82e3fe0768acd96644
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-30 23:26:55 -0600

    feat: add update main.jsx v1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ConfigProvider } from "antd";
+import { NotificationProvider } from "@/contexts/Notification";
+import { queryClient } from "@/config/queryClient";
+import { themeConfig } from "./config/theme";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider theme={themeConfig}>
+      <QueryClientProvider client={queryClient}>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ConfigProvider>
+  </StrictMode>
+);
