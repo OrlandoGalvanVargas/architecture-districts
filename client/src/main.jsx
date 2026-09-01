d28354feb010ebe941f1b3bcbc4439b7302a61ae
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-08-31 23:52:48 -0600

    refactor: update import path for themeConfig in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,3 @@
-// src/main.jsx
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,7 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ConfigProvider } from "antd";
 import { NotificationProvider } from "@/contexts/Notification";
 import { queryClient } from "@/config/queryClient";
-import { themeConfig } from "./config/theme";
+import { themeConfig } from "@/config/theme";
 import App from "./App";
 import "./index.css";
 
